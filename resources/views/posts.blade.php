a9f603080a865a4d0b8c2ebe4e2157fdc56c7d53
Elias Kirui <eliaskips1@gmail.com>
2023-06-27 09:27:21 +0300

    Add wildcard constaints and cache expensive operations

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        In vulputate tortor ligula, ut laoreet eros lacinia sed.
+        Nullam tristique, velit eget aliquam efficitur, lectus ligula
+        eleifend magna, sed vehicula metus erat eu dui. Cras hendrerit
+        tortor felis, non vulputate purus imperdiet at. Quisque ornare urna
+        vitae nibh laoreet scelerisque at a velit.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        In vulputate tortor ligula, ut laoreet eros lacinia sed.
+        Nullam tristique, velit eget aliquam efficitur, lectus ligula
+        eleifend magna, sed vehicula metus erat eu dui. Cras hendrerit
+        tortor felis, non vulputate purus imperdiet at. Quisque ornare urna
+        vitae nibh laoreet scelerisque at a velit.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        In vulputate tortor ligula, ut laoreet eros lacinia sed.
+        Nullam tristique, velit eget aliquam efficitur, lectus ligula
+        eleifend magna, sed vehicula metus erat eu dui. Cras hendrerit
+        tortor felis, non vulputate purus imperdiet at. Quisque ornare urna
+        vitae nibh laoreet scelerisque at a velit.
+    </p>
+</article>
+
+
+</body>
