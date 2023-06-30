6657a2e5265ab2a6f13761612fa636b5e3c003a4
Elias Kirui <eliaskips1@gmail.com>
2023-06-30 17:45:38 +0300

    Seed the database

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
@@ -21,7 +23,7 @@ Route::get('/', function () {
 //        logger($query->sql, $query->bindings);
 //    });
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -35,4 +37,11 @@ Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
     ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+            'posts' => $author->posts
+        ]);
+
 });
